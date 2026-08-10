40a65eedaf3ee97c91199f4031fc836f3c165bec
Madhuri Polisetty <madhuripolisetty@gmail.com>
2026-08-10 10:16:13 +0200

    created OUR PRODUCTS,DISCOVER, SAFETY&TECHNOLOGY BUTTONS FUNCTIONALITY

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-
 const HeroBanner = () => {
   return (
     <div className="w-full">
-      <Image
-        className="w-full"
-        src="/hero-banner.png"
-        alt="Honda Hero Banner"
-        width={1920}
-        height={300}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="https://edge.sitecorecloud.io/hondamotorc2d92_daworldf764-prod3600-398c/media/V2-Images/2FBanner-Videos/bannervideo_lowres.mp4"
       />
     </div>
   );
